5e47c3ee7e9b076c1455ce54b61308151aafe86c
Mr. Cortina <mr.cortina@Mrs-MacBook-Air.local>
2026-04-12 17:05:41 -0400

    fix logging

--- bot/bot.py
+++ bot/bot.py
@@ -17,7 +17,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("logs/bot.log"),
+        
         logging.StreamHandler()
     ]
 )
